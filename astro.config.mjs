4dcfac234583b483d2df85cb6541af7b19bee630
Magodelva25 <migudz@gmail.com>
2026-04-01 23:47:25 +0200

    corrección de fallos e incompatibilidades con touch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
+// Add `site: 'https://your-domain.com'` for correct canonical and OG URLs in production.
 export default defineConfig({
+  compressHTML: true,
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      cssMinify: true,
+    },
   },
 });
